910d7395488a0036f5b1ec9cf87332aaa04cdf6d
Lionel409 <depannfroidmoorea@gmail.com>
2025-11-24 16:33:59 -1000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
-import SignaturePage from './pages/SignaturePage.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/signer" element={<SignaturePage />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
